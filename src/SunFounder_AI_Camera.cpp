831ec55bc4d1d142aa350b444864cc517b240bdb
Cavon <cavonxx@gmail.com>
2023-06-15 18:36:02 +0800

    minor fix

--- src/SunFounder_AI_Camera.cpp
+++ src/SunFounder_AI_Camera.cpp
@@ -117,7 +117,7 @@ void AiCamera::loop() {
     }
     // recv WS+ data
     else if (IsStartWith(recvBuffer, WS_HEADER)) {
-      // debug("RX:"); debug(recvBuffer);
+      debug("RX:"); debug(recvBuffer);
       // DateSerial.print("RX:"); DateSerial.println(recvBuffer);
       ws_connected = true;
       this->subString(recvBuffer, strlen(WS_HEADER));
